5f6d50179fa8488dbd520cb4d9eb9d4576d2790c
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-28 12:58:57 -0400

    Final Commit with comment

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def on_button_pressed_a():
+    global numberOfPressesOnButtonA
+    numberOfPressesOnButtonA += 1
+    basic.show_string("" + str((numberOfPressesOnButtonA)))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global numberOfPressesOnButtonA
+    numberOfPressesOnButtonA += -1
+    basic.show_string("" + str((numberOfPressesOnButtonA)))
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+numberOfPressesOnButtonA = 0
+numberOfPressesOnButtonA = 0
+
 def on_forever():
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -6,6 +6,11 @@ input.onButtonPressed(Button.B, function () {
     numberOfPressesOnButtonA += -1
     basic.showString("" + (numberOfPressesOnButtonA))
 })
+// Created By: Vaughn Hatherall
+// 
+// Created on: Septermber 28
+// 
+// This program is Variables
 let numberOfPressesOnButtonA = 0
 numberOfPressesOnButtonA = 0
 basic.forever(function () {
