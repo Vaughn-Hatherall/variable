15f5bd9ef28585b3b0f3e73035a7ba0311de1c30
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-28 12:52:43 -0400

    Final commit

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    numberOfPressesOnButtonA += 1
+    basic.showString("" + (numberOfPressesOnButtonA))
+})
+input.onButtonPressed(Button.B, function () {
+    numberOfPressesOnButtonA += -1
+    basic.showString("" + (numberOfPressesOnButtonA))
+})
+let numberOfPressesOnButtonA = 0
+numberOfPressesOnButtonA = 0
 basic.forever(function () {
 	
 })
